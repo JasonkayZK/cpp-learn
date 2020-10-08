b36ea038bdcd511c657169740754d50b25ccd11e
jasonkayzk <271226192@qq.com>
2020-10-08 20:02:56 +0800

    add cJSON

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+
+#include "cJSON/cJSON.h"
 #include "math/MathUtils.h"
 
 int main() {
     hello(2);
     std::cout << power(2, 5) << std::endl;
+
+    char *post_str = nullptr;
+    cJSON *root = cJSON_CreateObject();
+
+    cJSON_AddStringToObject(root, "user", "jasonkay");
+    cJSON_AddStringToObject(root, "pwd", "hkcw3cjbc");
+    post_str = cJSON_Print(root);
+    cJSON_Delete(root);
+    root = nullptr;
+    printf("post_str is %s \n",post_str);
+
     return 0;
 }
