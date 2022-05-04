b13ab211d802bfd6d9b0e0669ac14ef3a3ec3f2d
JasonkayZK <271226192@qq.com>
2022-05-04 22:57:17 +0800

    finished: fmt

--- README.md
+++ README.md
@@ -1,36 +1,9 @@
-# **C++ Learn**
+# **Library fmt**
 
-一个从零开始学习C++的仓库；
+A branch to learn how to use [fmt](https://github.com/fmtlib/fmt)
 
-## **学习资料**
+## Reference
 
-- C++ primer plus(第六版)
+Linked blog:
 
-## **Jupyter分支**
-
-Jupyter 分支为：
-
-- [jupyter](https://github.com/JasonkayZK/cpp-learn/tree/jupyter)
-
-## **已完成项目**
-
-| Proj                                                        | Date       | INFO           |
-| ----------------------------------------------------------- |------------| -------------- |
-| [chapter3-type](https://github.com/JasonkayZK/cpp_learn/tree/chapter3-type) | 2020-11-15 | 学习C++基本类型 |
-| [chapter4-composed_type](https://github.com/JasonkayZK/cpp_learn/tree/chapter4-composed_type) | 2021-01-02 | 学习组合类型 |
-| [cmake](https://github.com/JasonkayZK/cpp_learn/tree/cmake) | 2020-10-08 | 学习CMake |
-| [RAII](https://github.com/JasonkayZK/cpp_learn/tree/raii) | 2021-01-02 | C++中RAII原理 |
-| [function-obj-wrapper](https://github.com/JasonkayZK/cpp_learn/tree/function-obj-wrapper) | 2021-01-02 | 学习函数包装器 |
-| [concurrency](https://github.com/JasonkayZK/cpp_learn/tree/concurrency) | 2021-01-02 | C++11/17/20并发编程 |
-| [dll](https://github.com/JasonkayZK/cpp_learn/tree/dll) | 2021-01-28 | DLL动态库生成 & 调用 |
-| [string](https://github.com/JasonkayZK/cpp_learn/tree/string) | 2021-05-07 | 学习C++中的string模板类 |
-| [smart-pointer](https://github.com/JasonkayZK/cpp_learn/tree/smart-pointer) | 2021-05-09 | 学习C++中的智能指针 |
-| [lib/boost](https://github.com/JasonkayZK/cpp-learn/tree/lib/boost) | 2022-04-11 | 学习 [boost](https://www.boost.org/) |
-| [proj/my-tiny-stl](https://github.com/JasonkayZK/cpp-learn/tree/proj/my-tiny-stl) | 2022-05-01 | 学习 [MyTinySTL](https://github.com/Alinshans/MyTinySTL) |
-
-## **其他说明**
-
-如何在Clion中执行单个c++文件：
-
--   [在Clion中执行单个Cpp文件](https://jasonkayzk.github.io/2020/11/15/在Clion中执行单个Cpp文件/)
-
+- [C++库之fmt](https://jasonkayzk.github.io/2022/05/04/C++库之fmt/)
--- main.cpp
+++ main.cpp
@@ -1,6 +1,97 @@
 #include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <fmt/core.h>
+#include <fmt/format.h>
+#include <fmt/chrono.h>
+#include <fmt/ranges.h>
+#include <fmt/os.h>
+#include <fmt/color.h>
+
+
+void simple_print() {
+    fmt::print("Hello, {}\n", "world!");
+}
+
+void string_format() {
+    std::string s = fmt::format("The answer is {}.", 42);
+    fmt::print("{}\n", s);
+}
+
+void float_format() {
+    fmt::print("The answer is {:.2f}\n", 1.12345678);
+}
+
+void position_param() {
+    fmt::print("I'd rather be {1} than {0}.\n", "right", "happy");
+}
+
+void named_param() {
+    fmt::print("Hello, {name}! The answer is {number}. Goodbye, {name}.\n",
+               fmt::arg("name", "World"), fmt::arg("number", 42));
+}
+
+void suffix_named_param() {
+    // #include <fmt/format.h> needed
+    using namespace fmt::literals;
+    fmt::print("Hello, {name}! The answer is {number}. Goodbye, {name}.\n",
+               "name"_a = "World", "number"_a = 22);
+}
+
+void time_format() {
+    // #include <fmt/chrono.h> needed
+    using namespace std::literals::chrono_literals;
+    fmt::print("Default format: {} {}\n", 42s, 100ms);
+    fmt::print("strftime-like format: {:%H:%M:%S}\n", 3h + 15min + 30s);
+}
+
+void collection_format() {
+    // #include <fmt/ranges.h> needed
+    std::vector<int> v = {1, 2, 3};
+    fmt::print("v: {}\n", v);
+
+    std::unordered_map<std::string, int> m{{"a", 1},
+                                           {"b", 2}};
+    fmt::print("m: {}\n", m);
+}
+
+void format_to_file() {
+    // #include <fmt/os.h> needed
+    auto out = fmt::output_file("test.txt");
+    out.print("Don't {}", "Panic");
+}
+
+void font_format() {
+    // #include <fmt/color.h> needed
+    fmt::print(fg(fmt::color::crimson) | fmt::emphasis::bold,
+               "Hello, {}!\n", "world");
+    fmt::print(fg(fmt::color::floral_white) | bg(fmt::color::slate_gray) |
+               fmt::emphasis::underline, "Hello, {}!\n", "мир");
+    fmt::print(fg(fmt::color::steel_blue) | fmt::emphasis::italic,
+               "Hello, {}!\n", "世界");
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    simple_print();
+
+    string_format();
+
+    float_format();
+
+    position_param();
+
+    named_param();
+
+    suffix_named_param();
+
+    time_format();
+
+    collection_format();
+
+    format_to_file();
+
+    font_format();
+
     return 0;
 }
