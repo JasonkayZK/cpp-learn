f42faea62f80899d0362d50ca9fcf0e763325350
JasonkayZK <271226192@qq.com>
2022-05-10 23:38:16 +0800

    finished: gtest

--- main_test.cc
+++ main_test.cc
@@ -7,6 +7,38 @@ TEST(HelloTest, PrintHello) {
     ASSERT_EQ(str.size(), 13);
 }
 
+TEST(ExpectAndAssert, ExpectTest) {
+    auto add = [](const int x, const int y) { return x + y; };
+
+    EXPECT_EQ(add(1, 2), 4);
+    EXPECT_EQ(add(1, 2), 3);
+}
+
+TEST(ExpectAndAssert, AssertTest) {
+    auto subtract = [](const int x, const int y) { return x - y; };
+
+//    ASSERT_EQ(subtract(3, 1), 3);
+    ASSERT_EQ(subtract(3, 1), 2);
+}
+
+
+TEST(TestMessage, Message) {
+    int result = 4;
+    EXPECT_EQ(1 + 2, result) << "1+2 should equals to: " << result;
+}
+
+class GlobalEvent : public testing::Environment {
+public:
+
+    void SetUp() override {
+        std::cout << "Before any case, Global" << std::endl;
+    }
+
+    void TearDown() override {
+        std::cout << "After all cases done, Global" << std::endl;
+    }
+};
+
 class VectorTest : public ::testing::Test {
 protected:
     // set resources before test
@@ -19,6 +51,14 @@ protected:
     // clean up resources after test
     void TearDown() override {
         vec.clear();
+    }
+
+    static void SetUpTestCase() {
+        std::cout << "SetUpTestCase()" << std::endl;
+    }
+
+    static void TearDownTestCase() {
+        std::cout << "TearDownTestCase()" << std::endl;
     }
 
     std::vector<int> vec;
@@ -38,6 +78,7 @@ TEST_F(VectorTest, Size) {
 
 int main(int argc, char **argv) {
     printf("Running main() from %s\n", __FILE__);
+    ::testing::AddGlobalTestEnvironment(new GlobalEvent); // add env
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
