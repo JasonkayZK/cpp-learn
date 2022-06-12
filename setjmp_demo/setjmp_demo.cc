a218de2b44291862f05bb20dfaceb3a51111371a
JasonkayZK <271226192@qq.com>
2022-06-12 17:46:47 +0800

    refactor

--- setjmp_demo/setjmp_demo.cc
+++ setjmp_demo/setjmp_demo.cc
@@ -13,16 +13,6 @@ jmp_buf Main;
 jmp_buf PointPing;
 jmp_buf PointPong;
 
-void Ping(void);
-void Pong(void);
-
-int main(int argc, char* argv[]) {
-    iter = 1;
-    if (setjmp(Main) == 0) Ping();
-    if (setjmp(Main) == 0) Pong();
-    longjmp(PointPing, 1);
-}
-
 void Ping(void) {
     if (setjmp(PointPing) == 0) longjmp(Main, 1); // 可以理解为重置，reset the world
     while (1) {
@@ -39,4 +29,11 @@ void Pong(void) {
         if (iter > max_iteration) exit(0);
         if (setjmp(PointPong) == 0) longjmp(PointPing, 1);
     }
+}
+
+int main(int argc, char* argv[]) {
+    iter = 1;
+    if (setjmp(Main) == 0) Ping();
+    if (setjmp(Main) == 0) Pong();
+    longjmp(PointPing, 1);
 }
