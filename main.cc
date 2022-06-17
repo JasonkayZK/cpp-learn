13cf2a12d357cc9c0050961116881376791c07e0
JasonkayZK <271226192@qq.com>
2022-06-17 15:44:38 +0800

    init: assembly

--- main.cc
+++ main.cc
@@ -1,6 +1,13 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    int foo = 10, bar = 15;
+    __asm__ __volatile__("add %%ebx,%%eax"
+            :"=a"(foo)
+            :"a"(foo), "b"(bar)
+            );
+    printf("foo+bar=%d\n", foo);
+
     return 0;
 }
