130ef080368aadabb31fb555e30ac0dd44f56cc4
jasonkayzk <271226192@qq.com>
2024-03-12 09:05:16 +0800

    finished: 3

--- 01_dll/3_reload/main/src/main.cc
+++ 01_dll/3_reload/main/src/main.cc
@@ -19,14 +19,14 @@ int main() {
   // Modify the source code and recompile the library.
   std::string filename = "./hello/src/hello.cc";
   // MacOS sed
-  std::string command = std::string("sed -i '' ") +
-//  std::string command = std::string("sed -i ") +
+//  std::string command = std::string("sed -i '' ") +
+  std::string command = std::string("sed -i ") +
       (bar == 200 ? "'s/200/300/'" : "'s/300/200/'") + " " +
       filename;
   system(command.c_str());
   // MacOS sed
-  command = std::string("sed -i '' ") +
-//  command = std::string("sed -i ") +
+//  command = std::string("sed -i '' ") +
+  command = std::string("sed -i ") +
       (bar == 200 ? "'s/Hi/Hello/'" : "'s/Hello/Hi/'") + " " + filename;
   system(command.c_str());
   system("cmake --build build");
